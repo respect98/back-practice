ee1af7c1a4ff0d72dc49d63cc9f3e435708c5543
respect <tmxhak12@naver.com>
2022-11-05 19:07:11 +0900

    no message

---
+++ chapter02/Ex2_08.java
@@ -0,0 +1,14 @@
+package chapter02;
+
+public class Ex2_08 {
+
+	public static void main(String[] args) {
+		System.out.println('\''); // '
+		System.out.println("abc\t123\b456"); // abc	12456
+		System.out.println('\n');
+		
+		System.out.println("\"Hello\"");//"Hello"
+		System.out.println("c:\\"); //c:\
+	}
+
+}
