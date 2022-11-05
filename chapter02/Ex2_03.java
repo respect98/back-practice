ccaf9b3ba1d5c5a1a3b4ffeef3fe9d99ca256b46
respect <tmxhak12@naver.com>
2022-11-05 19:06:32 +0900

    no message

---
+++ chapter02/Ex2_03.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+public class Ex2_03 {
+	public static void main(String arg[]) {
+		String name = "Ja"+"Va"; //java
+		String str = name+8.0 ;// "name" + "8.0" = java8.0
+		
+		System.out.println(name); // java
+		System.out.println(str);//java8.0
+		System.out.println(7+" "); //7 
+		System.out.println(" "+7);// 7
+		System.out.println(7+"");//7
+		System.out.println(""+7);//7
+		System.out.println(""+"");
+		System.out.println(7+7+""); //14
+		System.out.println(""+7+7); //"7"+7 "7"+"7" = 77
+	}
+}
