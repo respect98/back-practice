6d1c53a7f477a130868ba025b4aba5219ce16ff9
respect <tmxhak12@naver.com>
2022-11-06 18:06:49 +0900

    no message

---
+++ chapter03/Ex3_19.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+public class Ex3_19 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 8;
+		
+		System.out.printf("%d을 %d로 나누면, %n",x,y); // 10을 8로 나누면
+		System.out.printf("몫은 %d이고, 나머지는 %d입니다. %n",x/y,x%y); //몫은 1이고 나머지는 2입니다
+	}
+
+}
