92433647085ddbf4ef0bb0b957b5580583aa62e4
respect <tmxhak12@naver.com>
2022-11-06 18:06:34 +0900

    no message

---
+++ chapter03/Ex3_15.java
@@ -0,0 +1,12 @@
+package chapter03;
+
+public class Ex3_15 {
+
+	public static void main(String[] args) {
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase-32);
+		System.out.println(upperCase); //대문자 A가 a보다 코드값이 32 적음
+
+	}
+
+}
