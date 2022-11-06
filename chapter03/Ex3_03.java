d95284c9925a54e9775127c5ab605d8a6b3f7d01
respect <tmxhak12@naver.com>
2022-11-06 18:05:49 +0900

    no message

---
+++ chapter03/Ex3_03.java
@@ -0,0 +1,20 @@
+package chapter03;
+
+public class Ex3_03 {
+
+	public static void main(String[] args) {
+		int i=5, j=5;
+		System.out.println(i++); // print : 5 // i=6
+		System.out.println(++j); // print : 6 // j=6
+		System.out.println("i="+i+", j="+j); // i=6, j=6
+		
+		/*System.out.println(i);
+		 * i++;
+		 * ++j;
+		 * System.out.println(j);
+		 * */
+		
+		
+	}
+
+}
