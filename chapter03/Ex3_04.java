448f537cbf2a5502e0e2190c9dc430ec7bc85703
respect <tmxhak12@naver.com>
2022-11-06 18:05:54 +0900

    no message

---
+++ chapter03/Ex3_04.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+public class Ex3_04 {
+
+	public static void main(String[] args) {
+		int i = -10;
+		i = +i; //-10
+		System.out.println(i);
+		
+		i = -10;
+		i = -i; //10
+		System.out.println(i);
+	}
+
+}
